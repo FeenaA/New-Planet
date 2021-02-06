8a962aec02b133fdd001c28fb514640b3ce4177b
FeenaA <66133198+FeenaA@users.noreply.github.com>
2021-02-06 23:01:37 +0300

    rotation is correct

--- Assets/3Scripts/RoundMoving.cs
+++ Assets/3Scripts/RoundMoving.cs
@@ -11,7 +11,6 @@ public class RoundMoving : MonoBehaviour
     // угол отклонения, задается в градусах
     public float anlgeRotate;
 
-
     // текущее значение угла наклона
     private float phi1, phi2;
     // шаг для вращения Earth
@@ -21,7 +20,7 @@ public class RoundMoving : MonoBehaviour
     // Earth moving radius
     private float r_E;
 
-    private static System.Random rnd = new System.Random();
+    private readonly System.Random rnd = new System.Random();
 
     void Start()
     {
@@ -33,26 +32,16 @@ public class RoundMoving : MonoBehaviour
         // get distance between the centre of moveing and the object   
         r_E = SunEarthVector.magnitude;
 
-        // инициализация начального угла для Earth position
-        phi1 = 0;
-        // перевести из градусов в радианы
-        phi2 = anlgeRotate * Mathf.PI / 180f;
-
         step_phi = Mathf.PI / 2000f;
         step_phi *= rateRotate;
 
-        //-------------------------------------------
-        //getMaterial();
+        // инициализация начального угла для Earth position
+        int maxSteps = (int)(2f * Mathf.PI / step_phi);
+        phi1 = rnd.Next(0, maxSteps) * step_phi;
+        // перевести из градусов в радианы
+        phi2 = anlgeRotate * Mathf.Deg2Rad;
     }
 
-    private void getMaterial()
-    {
-        int L = getItems.sMaterials.Length - 1;
-        this.GetComponent<Renderer>().material = getItems.sMaterials[rnd.Next(0, L)];
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
         // вращение вокруг своей оси
@@ -61,26 +50,24 @@ public class RoundMoving : MonoBehaviour
             transform.rotation.eulerAngles.y + y_rotate, 
             transform.rotation.eulerAngles.z);
 
-
-        Vector3 CentrePos = gameObjectCentre.transform.position;
         // change Earth position
-        transform.position = gameObjectCentre.transform.position + posEarth();
-        //sunPos + posEarth(phi);
+        transform.position = gameObjectCentre.transform.position + PosEarth();
         phi1 += step_phi;
 
         // если Earth position совпала с startEarthPosition, начать подсчет шагов заново
         if (phi1 == 2*Mathf.PI) 
         {
-            // начать подсчет шагов заново
             phi1 = 0f;
         }
     }
 
-     Vector3 posEarth()
+     Vector3 PosEarth()
      {
+        float SinPhi1 = Mathf.Sin(phi1);
+
          Vector3  v = new Vector3 (
-                          r_E * Mathf.Sin(phi1) * Mathf.Cos(phi2),  
-                          r_E * Mathf.Sin(phi1) * Mathf.Sin(phi2),
+                          r_E * SinPhi1 * Mathf.Cos(phi2),  
+                          r_E * SinPhi1 * Mathf.Sin(phi2),
                           r_E * Mathf.Cos(phi1)
          ); 
 
